47e3637128bf9f6651c9c8d2aa58e62806ec6339
Jeff Dean <jeff@galvanize.com>
2016-05-12 15:43:09 -0600

    user name appears on refresh on home page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,6 +7,31 @@
        .when('/', {
         templateUrl: '/templates/home.html',
         controller: 'HomeController',
+        resolve: {
+          currentUser: function ($http) {
+
+            // if the browser has a token
+            if (localStorage.getItem('token')) {
+
+              // call /me endpoint, and pass it the token
+              const config = {
+                headers: {
+                  'Authorization': 'Bearer ' + localStorage.getItem('token')
+                }
+              }
+
+              return $http.get('/api/v1/users/me', config)
+                .then(function (response) {
+                  return response.data
+                })
+                .catch(function () {
+                  localStorage.clear();
+                  return null;
+                })
+
+            }
+          }
+        }
       })
       .when('/signup', {
         templateUrl: '/templates/signup.html',
